c6db641ddc282d52dd5f721ddc0374d6b5ce53e7
mr_christoff <chris.lawrence@gmx.com>
2016-09-13 08:52:38 +0000

    Challenge 7 error correction working, 7 complete

--- directory.rb
+++ directory.rb
@@ -10,7 +10,7 @@ def input_students_non_iteractive
   while !name.empty? do
     # add the student hash to the array
     student_template = 
-    students << {name: name, cohort: :november, hobbies: "being evil", gender: "prefer not to say"} 
+    students << {Name: name, Cohort: :november, Hobbie: "being evil", Gender: "prefer not to say"} 
     students.count == 1 ? plural_or_single = "student" : plural_or_single = "students"
     puts "Now we have #{students.count} #{plural_or_single}"
     # get another name from the user
@@ -20,13 +20,13 @@ def input_students_non_iteractive
   students
 end
 
-def input_students
+def input_students_interactive
   puts "Please enter the details of the student(s)"
   continue = true
   students = []
   while continue == true do
     # add the student hash to the array
-    student_template = {name: "To be confirmed", cohort: :November, hobbie: "To be confirmed", gender: "To be confirmed"} 
+    student_template = {Name: "To be confirmed", Cohort: :November, Hobbie: "To be confirmed", Gender: "To be confirmed"} 
     student_template.each do |key, value| # itterates the 'template' hash assigning the new value to the key selected
       puts "Please enter a #{key}"
       new_value = gets.chomp.capitalize.to_sym
@@ -44,6 +44,41 @@ def input_students
   students
 end
 
+def input_students_with_ammend
+  puts "Please enter the details of the student(s)"
+  continue = true
+  students = []
+  while continue == true do
+    # add the student hash to the array
+    student_template = {Name: "To be confirmed", Cohort: :November, Hobbie: "To be confirmed", Gender: "To be confirmed"} 
+    student_template.each do |key, value| # itterates the 'template' hash assigning the new value to the key selected
+      puts "Please enter a #{key}"
+      new_value = gets.chomp.capitalize.to_sym
+      student_template[key] = new_value if new_value != :""
+    end
+    # add template to student array
+    students << student_template
+    student = students[-1]
+    # check informatin is correct
+    puts "\nPlease check this information is correct:"#.center(100)
+    puts ""
+    puts "#{student[:Name]} (#{student[:Cohort]} Cohort) Hobby: #{student[:Hobbie]}, Gender: #{student[:Gender]}.\n"#.center(100)
+    puts ""
+    puts "would you like to delete the student and data?"
+    puts "Type 'R' to remove student data or any key to continue"
+    answer = gets.chomp.capitalize
+    students.pop if answer == "R" 
+
+
+    students.count == 1 ? plural_or_single = "student" : plural_or_single = "students"
+    puts "Now we have #{students.count} #{plural_or_single}"
+    puts "continue to add students? Y/N"
+    gets.chomp.upcase == "Y" ? continue = true : continue = false
+
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villans Academy".center(100)
@@ -53,23 +88,23 @@ end
 
 def print(students)
   students = students.each_with_index do |student, index|
-      puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)"
+      puts "#{index + 1}.#{student[:Name]} (#{student[:Cohort]} Cohort)"
   end
 end
 
 
 def print_students_begining_with(students) # only print students whos names begin with "D" or "d"
   students = students.each_with_index do |student, index|
-  	if (student[:name])[0] == "D" || (student[:name])[0] == "d"
-  		puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)"
+  	if (student[:Name])[0] == "D" || (student[:Name])[0] == "d"
+  		puts "#{index + 1}.#{student[:Name]} (#{student[:Cohort]} Cohort)"
   		end
   	end
 end
 
 def print_students_with_names_less_than(students) # only print students who's names are shorter than 12 characters
   students = students.each_with_index do |student, index|
-  	if (student[:name]).length < 12 
-  		puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort)"
+  	if (student[:Name]).length < 12 
+  		puts "#{index + 1}.#{student[:Name]} (#{student[:Cohort]} Cohort)"
   		end
   	end
 end
@@ -78,7 +113,7 @@ def print_without_each(arr) # print without using the 'each' method, and use loo
   n = 0
   until n == arr.length
     student = arr[n]
-    puts "#{n + 1}.#{student[:name]} (#{student[:cohort]} cohort) Hobby: #{student[:hobbies]} Gender: #{student[:gender]}".center(100)
+    puts "#{n + 1}.#{student[:Name]} (#{student[:Cohort]} Cohort) Hobby: #{student[:Hobbie]} Gender: #{student[:Gender]}".center(100)
     n += 1
   end
 end
@@ -91,8 +126,7 @@ def print_footer(names)
 end
 
 # nothing happens until we call the mothods
-students = input_students
+students = input_students_with_ammend
 print_header
 print_without_each(students)
 print_footer(students)
-
